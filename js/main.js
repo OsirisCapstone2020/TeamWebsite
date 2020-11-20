b5a71efbc36431fef8395c492277e92451649a8c
Evin Dunn <evindunn14@gmail.com>
2020-11-20 13:28:28 -0700

    Updates desc.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 function onTabChanged(tabUrl) {
     $("#content").load(tabUrl);
+
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("tab", tabUrl.replace(".html", ""));
+    window.history.pushState({}, "", `?${searchParams.toString()}`);
 }
 
 // Load home when site starts
-window.addEventListener('load', () => onTabChanged('./pages/home.html'));
+window.addEventListener('load', () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const tab = searchParams.get("tab");
+    if (tab) {
+        onTabChanged(`${tab}.html`);
+    }
+});
